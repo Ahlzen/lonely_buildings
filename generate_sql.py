840f3269ffef35f8a268fc4d1ef7db3d3bf24a1a
Ahlzen <lars@ahlzen.com>
2013-06-12 21:57:51 -0400

    Added initial version

---
+++ generate_sql.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import pystache
+
+basename = 'find_buildings'
+config = {
+  'SRC_PREFIX': 'ma',  # prefix of source table, e.g. "planet_osm"
+  'DEST_PREFIX': 'lonely' # prefix of desination tables, e.g. "lonely"
+  }
+        
+# Run template and write sql
+with open(basename + '.templ','r') as infile:
+  srctext = infile.read()
+sql = pystache.render(srctext, config)
+with open(basename + '.sql','w') as outfile:
+  outfile.write(sql)
+
